0501d916e323b4d056b8f869c46e19f8945dc151
marco-vidali <marcovidali@outlook.com>
2025-04-17 10:11:56 +0200

    Add sir_cat_enters animation

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+@onready var animation_player = $AnimationPlayer
+
+func _on_signal(_signal):
+    animation_player.play(_signal)
+
 func _ready():
     Dialogic.start("sir_cat_meets_miss_duck")
+    Dialogic.signal_event.connect(_on_signal)
