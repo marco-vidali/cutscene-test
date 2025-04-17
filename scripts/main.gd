170400c97042b53de303c744abf5f69dd7a04717
marco-vidali <marcovidali@outlook.com>
2025-04-17 10:22:05 +0200

    Hide dialogue box while animation is playing

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,12 +3,14 @@ extends Node2D
 @onready var animation_player = $AnimationPlayer
 
 func _on_signal(_signal):
+    Dialogic.Text.hide_textbox()
     Dialogic.paused = true
 
     animation_player.play(_signal)
     await animation_player.animation_finished
 
     Dialogic.paused = false
+    Dialogic.Text.show_textbox()
 
 func _ready():
     Dialogic.start("sir_cat_meets_miss_duck")
