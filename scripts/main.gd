fe7515a33489b83ea24c8500b95c47fbca3a5062
marco-vidali <marcovidali@outlook.com>
2025-04-17 10:17:10 +0200

    Add dialogue text

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,12 @@ extends Node2D
 @onready var animation_player = $AnimationPlayer
 
 func _on_signal(_signal):
+    Dialogic.paused = true
+
     animation_player.play(_signal)
+    await animation_player.animation_finished
+
+    Dialogic.paused = false
 
 func _ready():
     Dialogic.start("sir_cat_meets_miss_duck")
